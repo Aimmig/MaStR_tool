9449b6b3113a279250f5152a020d358758c2e680
Andre Immig <an_drimg@protonmail.com>
2026-01-08 10:36:46 +0100

    Refactoring

--- main.py
+++ main.py
@@ -1,48 +1,114 @@
 from open_mastr import Mastr
 import pandas as pd
+from datetime import date
 
-used_cols={
+# map to translate only the acutally used columns
+used_cols = {
         'Technologie': 'technology', 'WindAnLandOderAufSee': 'on_or_offshore',
-        'Bundesland': 'state' ,'Landkreis': 'county', 'Gemeinde': 'municipality',
-        'GeplantesInbetriebnahmedatum': 'opening_date', 'Inbetriebnahmedatum': 'start_date',
-        'DatumEndgueltigeStilllegung': 'end_date', 'DatumDownload': 'check_date',
-        'NameWindpark': 'name_windfarm', 'Nettonennleistung': 'output', 'NameStromerzeugungseinheit': 'name_unit',
-        'Laengengrad': 'lon', 'Breitengrad': 'lat', 'EegMastrNummer': 'ref:eeg',
-        'Hersteller': 'manufacturer', 'Typenbezeichnung': 'model', 'Nabenhoehe': 'height:hub', 'Rotordurchmesser': 'rotor:diameter'
+        'Bundesland': 'state', 'Landkreis': 'county',
+        'Gemeinde': 'municipality',
+        'GeplantesInbetriebnahmedatum': 'opening_date',
+        'Inbetriebnahmedatum': 'start_date',
+        'DatumEndgueltigeStilllegung': 'end_date',
+        'DatumDownload': 'check_date',
+        'NameWindpark': 'name_windfarm', 'Nettonennleistung': 'output',
+        'NameStromerzeugungseinheit': 'name_unit',
+        'Laengengrad': 'lon', 'Breitengrad': 'lat',
+        'EegMastrNummer': 'ref:eeg',
+        'Hersteller': 'manufacturer', 'Typenbezeichnung': 'model',
+        'Nabenhoehe': 'height:hub', 'Rotordurchmesser': 'rotor:diameter'
         }
 
+# columns used for printing/debuging
 print_cols = [
         'lon', 'lat',
         'output', 'name_unit', 'ref:eeg',
         'opening_date', 'start_date', 'end_date'
 ]
 
-# download relevant data
-db = Mastr()
-db.download(data="wind",api_data_types=["unit_data"],api_location_type=["location_elec_generation"])
+today = date.today().isoformat()
 
-# generate a list of all tables
-table="wind_extended"
-df = pd.read_sql_query("SELECT name FROM sqlite_master WHERE type='table';", con=db.engine)
-df = pd.read_sql(sql=table, con=db.engine)
 
-#rename columns to better match osm tags
-df = df.rename(columns=used_cols)
+# select only data within some area
+def filter_region(df, state=None, county=None, municipality=None):
+    if state:
+        df = df[df["state"] == state]
+    if county:
+        df = df[df["county"] == county]
+    if municipality:
+        df = df[df["municipality"] == municipality]
+    return df
 
-# filter data before further processing 
-df.dropna(axis=1,how='all',inplace=True)
 
-df = df[list(used_cols.values())]
-# only onshore regular power plants considered
-df = df.loc[
-    (df["on_or_offshore"] == "Windkraft an Land") &
-    (df["technology"] == "Horizontalläufer") &
-    (df["output"] > 600)]
+def get_prefiltered_Mastr(on_or_offshore="Windkraft an Land",
+                          technology="Horizontalläufer", output=600):
 
-df = df[df["state"] == "Rheinland-Pfalz"]
-#df = df[df["municipality"] == "Gau-Bickelheim"]
-#df = df[df["end_date"].notnull()]
-#df = df[df["end_date"] >= "2024-01-01"]
-df = df[df["opening_date"].notnull()]
-#df = df[df["start_date"].notnull()]
-print(df[print_cols])
+    # download relevant data
+    db = Mastr()
+    db.download(data="wind", api_data_types=["unit_data"],
+                api_location_type=["location_elec_generation"])
+
+    # generate a list of all tables
+    table = "wind_extended"
+    df = pd.read_sql_query(
+            "SELECT name FROM sqlite_master WHERE type='table';",
+            con=db.engine)
+    df = pd.read_sql(sql=table, con=db.engine)
+    # rename columns to better match osm tags
+    df = df.rename(columns=used_cols)
+
+    # filter data before further processing
+    df.dropna(axis=1, how='all', inplace=True)
+
+    df = df[list(used_cols.values())]
+    # filter according to given or default values which are considered
+    df = df.loc[
+        (df["on_or_offshore"] == on_or_offshore) &
+        (df["technology"] == technology) &
+        (df["output"] > output)]
+    return df
+
+
+def get_plants_with_end_date(df):
+    return df[df["end_date"].notnull()]
+
+
+def get_plants_with_start_date(df):
+    return df[df["start_date"].notnull()]
+
+
+def get_plants_with_opening_date(df):
+    return df[df["opening_date"].notnull()]
+
+
+def get_plants_with_future_end_date(df):
+    return df[df["end_date"] > today]
+
+
+def get_plants_with_past_end_date(df):
+    return df[df["end_date"] < today]
+
+
+def get_plants_with_future_opening_date(df):
+    return df[df["opening_date"] > today]
+
+
+def get_plants_with_past_opening_date(df):
+    return df[df["opening_date"] < today]
+
+
+def get_plants_with_past_start_date(df):
+    return df[df["start_date"] < today]
+
+
+# only for completion this should always return an empty df
+def get_plants_with_future_start_date(df):
+    return df[df["start_date"] > today]
+
+
+if __name__ == '__main__':
+    df = get_prefiltered_Mastr()
+    df = filter_region(df, state="Rheinland-Pfalz")
+    # df = get_plants_with_past_start_date(df)
+    df = get_plants_with_past_end_date(df)
+    print(df[print_cols])
