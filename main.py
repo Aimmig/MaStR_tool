b196f1781d93d117cf227fc77c7e31e4e90c8b49
Andre Immig <an_drimg@protonmail.com>
2026-01-08 16:38:59 +0100

    Add documentation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from datetime import date
 
 # map to translate only the acutally used columns
+# might be adapted to include more data or to throw away unwanted columns
 used_cols = {
         'Technologie': 'technology', 'WindAnLandOderAufSee': 'on_or_offshore',
         'Bundesland': 'state', 'Landkreis': 'county',
@@ -14,23 +15,34 @@ used_cols = {
         'NameWindpark': 'name_windfarm', 'Nettonennleistung': 'output',
         'NameStromerzeugungseinheit': 'name_unit',
         'Laengengrad': 'lon', 'Breitengrad': 'lat',
-        'EegMastrNummer': 'ref:eeg',
+        'EinheitMastrNummer': 'ref:MaStR',
         'Hersteller': 'manufacturer', 'Typenbezeichnung': 'model',
         'Nabenhoehe': 'height:hub', 'Rotordurchmesser': 'rotor:diameter'
         }
 
 # columns used for printing/debuging
 print_cols = [
-        'lon', 'lat',
-        'output', 'name_unit', 'ref:eeg',
+        'lon', 'lat', 'output', 'ref:MaStR',
         'opening_date', 'start_date', 'end_date'
 ]
 
 today = date.today().isoformat()
 
 
-# select only data within some area
 def filter_region(df, state=None, county=None, municipality=None):
+    """
+    Filter data by some regional property
+
+    Parameters:
+    df: The dataframe to filter on.
+    state: The state which should be included.
+    county: The county which should be included.
+    municipality: The municipality which should be included.
+
+    Returns:
+    The filtered dataframe
+    """
+
     if state:
         df = df[df["state"] == state]
     if county:
@@ -43,7 +55,22 @@ def filter_region(df, state=None, county=None, municipality=None):
 def get_prefiltered_Mastr(on_or_offshore="Windkraft an Land",
                           technology="Horizontalläufer", output=600):
 
-    # download relevant data
+    """
+    Downloads the Mastr unit data and filters by the given technolgy,
+    On/Offshore and power output. Translates the colums to be shorter names
+    and more closer to usefull osm tags. Large parts of data is thrown away
+    since it's not really relevant, also throws away lot's of empty columns
+    which are always empty for wind power plants.
+
+    Parameters:
+    on_or_offshore: either "Windkraft an Land" or "Windkraft auf See"
+    technoly: either "Horizontalläufer" or "Vertikalläufer"
+    output: the nominal power output of the plant. Default no small plants.
+
+    Returns: Dataframe which contains the pre filtered Mastr
+    """
+
+    # download relevant data with api
     db = Mastr()
     db.download(data="wind", api_data_types=["unit_data"],
                 api_location_type=["location_elec_generation"])
@@ -54,13 +81,13 @@ def get_prefiltered_Mastr(on_or_offshore="Windkraft an Land",
             "SELECT name FROM sqlite_master WHERE type='table';",
             con=db.engine)
     df = pd.read_sql(sql=table, con=db.engine)
+
     # rename columns to better match osm tags
     df = df.rename(columns=used_cols)
-
     # filter data before further processing
     df.dropna(axis=1, how='all', inplace=True)
-
     df = df[list(used_cols.values())]
+
     # filter according to given or default values which are considered
     df = df.loc[
         (df["on_or_offshore"] == on_or_offshore) &
@@ -69,46 +96,61 @@ def get_prefiltered_Mastr(on_or_offshore="Windkraft an Land",
     return df
 
 
+# ---- Basic filters based on NaT ------
+
 def get_plants_with_end_date(df):
+    """Return only plants with known end_date.
+    (Planed) Decommissioning can be safely assumed.
+    This date can safely be assumed to be in the past.
+    """
     return df[df["end_date"].notnull()]
 
 
 def get_plants_with_start_date(df):
+    """Return only plants with known start_date.
+    This date can safely be assumed to be in the past.
+    Note this includes plant that already out of operation again.
+    """
     return df[df["start_date"].notnull()]
 
 
 def get_plants_with_opening_date(df):
+    """Return only plants with known opening_date.
+    Note this might include plants which should have openend,
+    but still are not in operation."""
     return df[df["opening_date"].notnull()]
 
 
-def get_plants_with_future_end_date(df):
-    return df[df["end_date"] > today]
-
-
-def get_plants_with_past_end_date(df):
-    return df[df["end_date"] < today]
-
+# ---- Basic filters based on comparison with todays date -----
 
 def get_plants_with_future_opening_date(df):
+    """Return only plants which are expected to open."""
     return df[df["opening_date"] > today]
 
 
 def get_plants_with_past_opening_date(df):
+    """Returns only plants that should have opened
+    but still aren't operational.
+    This could mean any form delay, or it was never built at all.
+    """
     return df[df["opening_date"] < today]
 
 
-def get_plants_with_past_start_date(df):
-    return df[df["start_date"] < today]
-
-
-# only for completion this should always return an empty df
-def get_plants_with_future_start_date(df):
-    return df[df["start_date"] > today]
+def get_plants_currently_operational(df):
+    """Returns only plants that have started and if end_date exists
+    it is still in the future.
+    """
+    df = df.loc[
+        (df["opening_date"].isnull()) &
+        (df["end_date"].isnull())]
+    return df
 
 
 if __name__ == '__main__':
     df = get_prefiltered_Mastr()
-    df = filter_region(df, state="Rheinland-Pfalz")
-    # df = get_plants_with_past_start_date(df)
-    df = get_plants_with_past_end_date(df)
+    # df = filter_region(df, state="Rheinland-Pfalz")
+    df = get_plants_currently_operational(df)
+    df.sort_values("start_date", inplace=True)
+    # df = get_plants_with_end_date(df)
+
     print(df[print_cols])
