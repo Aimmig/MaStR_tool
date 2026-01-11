c021a3416f51c26d16c8599200781fa978dc82fe
Andre Immig <an_drimg@protonmail.com>
2026-01-11 21:53:52 +0100

    Add more common manufacturers

--- utils/Constants.py
+++ utils/Constants.py
@@ -1,21 +1,36 @@
 
 # Note: some manufactures have merged or renamed
 # so this is not working in every case
+# Notably: Senvion ...
 MANUFACTURERS = {"Nordex": "Nordex",
                  "ENERCON": "Enercon",
                  "Vestas": "Vestas",
                  "Fuhrländer": "Fuhrländer",
                  "GE Wind Energy": "GE Wind Energy",
+                 "GE Renewable Germany": "GE Renewable Germany",
                  "NEG Micon": "NEG Micon",
                  "Südwind": "Südwind",
                  "Schütz": "Schütz",
                  "Kenersys": "Kenersys",
                  "Siemens Gamesa": "Siemens Gamesa",
-                 # "Gamesa": "Gamesa",
+                 "Gamesa Corporación Tecnológica": "Gamesa",
+                 "Siemens Windenergie GmbH" : "Siemens Windenergie GmbH"
                  "REpower": "REpower",
                  "DeWind": "DeWind",
                  "Senvion": "Senvion",
-                 "Tacke": "Tacke"
+                 "Tacke": "Tacke",
+                 "Frisia": "Frisia",
+                 "Vensys": "Vensys",
+                 "Nordtank": "Nordtank",
+                 "Enron": "Enron",
+                 "General Electric": "General Electric",
+                 "eno": "eno",
+                 "AN Windenergie": "AN Windenergie",
+                 "Jacobs": "Jacobs",
+                 "HSW": "HSW",
+                 "Lagerwey": "Lagerwey"
+                 "Amperax": "Amperax",
+                 "Adwen": "Adwen"
                  }
 
 COMMON_COLS = {"Laengengrad": "lon",
