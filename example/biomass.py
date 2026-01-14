aafa766e330df87edd9dc62f9158b55f0a8bf637
Andre Immig <an_drimg@protonmail.com>
2026-01-14 19:56:52 +0100

    Add example query strings

--- example/biomass.py
+++
@@ -1,8 +0,0 @@
-    query_string = "Nettonennleistung > 30"
-    plants = plants.query(query_string)
-    query_string = "Bundesland == 'Rheinland-Pfalz' and InstallierteLeistung > 300 and Technologie == 'Horizontalläufer'"
-    plants = plants.query(query_string)
-    query_string = "Technologie == 'Verbrennungsmotor' and Energietraeger == 'Klärschlamm'"
-    plants = plants.query(query_string)
-    query_string = "ArtDerWasserkraftanlage == 'Laufwasseranlage' and Bruttoleistung > 100"
-    plants = plants.query(query_string)
