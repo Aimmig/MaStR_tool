dd3de28558b5f5c20037af73bea9c88aca44c764
Andre Immig <an_drimg@protonmail.com>
2026-09-21 19:28:07 +0200

    upgrade version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="mastr_osm_utils",
-    version="0.0.1",
+    version="0.0.2",
     description="Download, filter MaStRData and compare to existing osm data",
     packages=find_packages(),
     install_requires=requirements
