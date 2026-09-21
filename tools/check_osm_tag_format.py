aa7380981338f2ae23255e12677e93bb25a958b6
Andre Immig <an_drimg@protonmail.com>
2026-09-21 19:45:22 +0200

    Fix check osm script

--- tools/check_osm_tag_format.py
+++ tools/check_osm_tag_format.py
@@ -1,15 +1,15 @@
 from utils.PreConfiguredParser import createOSMFormatParser
 from utils.PlantsFromOSM import getWindPlantsInArea
 from utils.CheckOsmTagFormats import check_tags
-
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
+    load_dotenv("env_conf/.chech_osm_env")
     parser = createOSMFormatParser()
     args = parser.parse_args()
     check_col = args.tag
     osm_units = getWindPlantsInArea(args.area,
-                                    sanitize=True,
-                                    invalidate_cache=True)
+                                    sanitize=True)
     filtered, cols = check_tags(osm_units, check_col, strict=True)
     output = None
     csv = filtered[cols].to_csv(
