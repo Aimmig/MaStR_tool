fc13e748116d24f56582c57f08b205e1f0171761
Andre Immig <an_drimg@protonmail.com>
2026-09-22 21:05:31 +0200

    Check also for construction in description and for planned power

--- tools/check_osm_life_cycle.py
+++ tools/check_osm_life_cycle.py
@@ -3,6 +3,16 @@ from utils.PlantsFromOSM import getWindPlantsInArea
 from dotenv import load_dotenv
 from get_mastr_data_by_ref import get_data
 
+CONSTRUCTION_POWER = "construction:power"
+PLANNED_POWER = "planned:power"
+DESC_CONSTR = "under construction"
+REF_MASTR = "ref:mastr"
+
+
+def filter_for_construction(df):
+    tags = df.loc[(df[CONSTRUCTION_POWER] == 'generator') | (df['description'] == DESC_CONSTR) | (df[PLANNED_POWER] == 'generator')]
+    return tags[REF_MASTR].dropna().to_list()
+
 if __name__ == "__main__":
     env_file = "env_conf/.check_osm_life_cyle_env"
     load_dotenv(env_file)
@@ -10,10 +20,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     osm_units = getWindPlantsInArea(args.area,
                                     sanitize=True)
-    CONSTRUCTION_POWER = "construction:power"
-    REF_MASTR = "ref:mastr"
-    tags = osm_units[[REF_MASTR, CONSTRUCTION_POWER]].dropna(subset=[REF_MASTR, CONSTRUCTION_POWER])
-    refs = tags[REF_MASTR].to_list()
     cols = ["Inbetriebnahmedatum", "Laengengrad", "Breitengrad"]
+    refs = filter_for_construction(osm_units)
     construction_now_open = get_data("wind", refs, cols)
     print(construction_now_open)
